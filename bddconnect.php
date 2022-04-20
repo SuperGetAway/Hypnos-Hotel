8e1ddc3f376dc5760387afd23fad1e2c0d294693
Get <94855437+SuperGetAway@users.noreply.github.com>
2022-04-20 17:59:38 +0200

    changements final
    j'ai du retourner sur un html vue que mon PHP ne fonctionner pas (lire le doc)

--- bddconnect.php
+++ bddconnect.php
@@ -1,17 +1,16 @@
-<? 
-$host = 'office.ntclick.com:33306'; 
-$db = 'hypnos';
-$user ='root';
-$passdb = 'a15bA4De1f';
+<?php 
+define("DBHOST","office.ntclick.com:33306");
+define("DBUSER","root");
+define("DBPASS","a15bA4De1f");
+define("DBNAME","hypnos");
 
-$charset = 'utf8mb4';
-$dsn = "mysql:host=$host;ddbname=$db;charset=$charset";
-$option = [
-PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-PDO::ATTR_EMULATE_PREPARES => false
-];
 
-$pdo = new PDO($dsn,$user,$passdb,$options)
+function ppp ()
+{
 
+  $con = mysqli_connect('office.ntclick.com:33306','root','a15bA4De1f','hypnos');
+$sql = "insert into user ('prenom','nom','mail','password','radio') values ('q','s','d','f',1) ";
+
+$result = mysqli_query($con, $sql);  
+}
 ?>
